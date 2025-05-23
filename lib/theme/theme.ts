73a33ded0567429032804f9e2f7faa66b511d12c
Hashim Jacobs <hashjaco@gmail.com>
2025-05-23 10:18:01 -0700

    Add breakpoints

--- lib/theme/theme.ts
+++ lib/theme/theme.ts
@@ -10,10 +10,15 @@ const config = defineConfig({
 
     theme: {
         breakpoints: {
+            xxs: "10em", // 160px
+            xs: "20em", // 320px
             sm: "30em", // 480px
             md: "48em", // 768px
             lg: "62em", // 992px
             xl: "80em", // 1280px
+            "2xl": "96em", // 1536px
+            "3xl": "120em", // 1920px
+            "4xl": "144em", // 2304px
         },
         tokens: {
             colors: {
